9cd682d67f4a625fc97c455a1f7c888cc65460ef
rpaul <rpaul93@wgu.edu>
2024-08-04 19:07:10 -0400

    Adds Graph display

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  //adds a state to keep track if the graph should be shown
+  showGraph: boolean,
 }
 
 /**
@@ -22,6 +24,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false,
     };
   }
 
@@ -29,18 +32,35 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+
+    //reads current state of showGraph boolean to determine if graph should be rendered
+    if(this.state.showGraph){
+      return (<Graph data={this.state.data}/>)
+    }
+
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+
+
+    let x = 0;
+
+    //sets interval at which the graph should get data
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) =>{
+        this.setState({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x > 1000){
+        clearInterval(interval)
+      }
+    }, 100)
   }
 
   /**
